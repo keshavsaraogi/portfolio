effd7c9bf6b1634f012fd947a4564d50af8a2a1a
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-04-09 17:40:26 +0530

    Updates

---
+++ precache-manifest.25987c50d862b93a80543ad489a1d8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420c5b632b6c973f898eb2644ae2db33",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "613f3aecd8013d25cbf2",
+    "url": "/portfolio/static/css/main.caae00ce.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/portfolio/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "613f3aecd8013d25cbf2",
+    "url": "/portfolio/static/js/main.a2245efc.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4c45bb6761d70ce405ca5894f5ecd84d",
+    "url": "/portfolio/static/media/image.4c45bb67.jpeg"
+  }
+]);
