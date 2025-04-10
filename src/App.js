c9f9272df7909b9627395ee856ff1d47cb0180b8
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-04-10 07:20:35 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../src/components/Navbar';
 import Hero from '../src/components/Hero';
+import About from '../src/components/About';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
